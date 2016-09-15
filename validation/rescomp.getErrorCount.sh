4cd9e85eb45f75c89d176094f990b90096f270eb
Joe Zhu <sha.joe.zhu@gmail.com>
2016-09-15 13:41:15 +0100

    update run script

--- validation/rescomp.getErrorCount.sh
+++ validation/rescomp.getErrorCount.sh
@@ -8,7 +8,7 @@
 while read sample ; do
 for seed in $(seq 1 15);
 do
-prefix="/well/mcvean/joezhu/pf3k/pf3k_5_1_final/"${sample}/${sample}_seed${seed}k3
+prefix="/well/mcvean/joezhu/pf3k/pf3k_5_1_final/dEploidOut/"${sample}/${sample}_seed${seed}k3
 R --slave "--args ${prefix}" < tmp3.r
 done
 done < labSampleNames3Strains
